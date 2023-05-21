2161792b4980fcb608687c24cdf5a11cb2289475
SabSVL <sabrina.venancio.luiz@gmail.com>
2023-05-21 19:11:14 -0300

    Criacao da classe assinante e seus atributos

---
+++ EmpresaDeTelefoniaCelular - TP1/src/Assinante.java
@@ -0,0 +1,8 @@
+
+public class Assinante {
+	private long cpf; // CPF do assinante
+	private String nome; // nome completo do assinante
+	private int numero; //  numero do telefone celular do assinante
+	protected Chamada[] chamadas; // vetor de referencias a objetos da classe Chamada
+	protected int numChamadas; // n�mero de chamadas feitas pelo assinante
+}
