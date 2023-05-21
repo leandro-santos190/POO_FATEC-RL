c476e8e99a30f434298574e666a4978bb9d29d2f
Isabelly Santos <isabelly.santos885@gmail.com>
2023-05-21 20:14:18 -0300

    Iniciando o construtor

--- EmpresaDeTelefoniaCelular - TP1/src/Chamada.java
+++ EmpresaDeTelefoniaCelular - TP1/src/Chamada.java
@@ -1,8 +1,13 @@
-import java.util.GregorianCalendar;
-import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar; // Importando a classe  
+import java.text.SimpleDateFormat; // Importando classe que formata as datas
 
-public class Chamada {
+public class Chamada { // Inicializando a classe chamada com seus atributos
   private GregorianCalendar data;
   private int duracao;
+
+  public Chamada(GregorianCalendar data, int duracao) { // Iniciando o construtor da classe
+    this.data = data;
+    this.duracao = duracao;
+  }
 
 }
