00c0472c30d0f3b05a70df2e79e4340cf5711ca6
leandro-santos190 <leandro.santos190@fatec.sp.gov.br>
2023-05-21 18:29:10 -0300

    Criação do método fazerChamada para PosPago
    criação do método fazerChamada, adicionando as informações no vetor de chamadas, e incrementando o contador de chamadas.

--- EmpresaDeTelefoniaCelular - TP1/src/PosPago.java
+++ EmpresaDeTelefoniaCelular - TP1/src/PosPago.java
@@ -1,3 +1,4 @@
+import java.util.GregorianCalendar;
 
 public class PosPago extends Assinante {
 
@@ -8,7 +9,24 @@ public class PosPago extends Assinante {
 		this.assinatura = assinatura;
 	}
 
-	
+	public void fazerChamada(GregorianCalendar data, int duracao) {
+
+		// System.out.println("Tamanho do vetor chamadas = "+chamadas.length);
+		// System.out.println("numChamadas = "+numChamadas);
+
+		if (chamadas.length > numChamadas && this.assinatura > 0) {
+			Chamada chamada = new Chamada(data, duracao); //instanciei um objeto do tipo Chamada, passando como argumento, os parametros recebidos no método
+			chamadas[numChamadas] = chamada;//adiciono as informações da chamada, no vetor chamadas 
+			numChamadas++;//incremento o número de chamadas feita usando plano pós-pago
+			System.out.println("Chamada realizada com sucesso!");//Saída para indicar que a chamada foi bem sucedidida
+
+		} else {
+
+			System.out.println("Não foi possível realizar a chamada!");//Saída para indicar que a chamada não foi bem sucedidida
+
+		}
+	}
+
 	
 	
 
