9b4236f6dcaf51c09eff946b1032463dff909ce6
SabSVL <sabrina.venancio.luiz@gmail.com>
2023-05-21 19:11:41 -0300

    criacao do metodo constructor

--- EmpresaDeTelefoniaCelular - TP1/src/Assinante.java
+++ EmpresaDeTelefoniaCelular - TP1/src/Assinante.java
@@ -5,4 +5,13 @@ public class Assinante {
 	private int numero; //  numero do telefone celular do assinante
 	protected Chamada[] chamadas; // vetor de referencias a objetos da classe Chamada
 	protected int numChamadas; // n�mero de chamadas feitas pelo assinante
+
+	public Assinante(long cpf, String nome, int numero) {
+		this.cpf = cpf;
+		this.nome = nome;
+		this.numero = numero;
+		this.chamadas = new Chamada[10];
+		this.numChamadas++;
+	}
+
 }
