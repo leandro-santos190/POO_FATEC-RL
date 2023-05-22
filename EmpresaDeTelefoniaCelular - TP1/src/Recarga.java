be175cf1187044f5159fb1b1a6119b2d73dae67e
Thaís Dias <thaiisdiias15@gmail.com>
2023-05-21 22:47:31 -0300

    Classe Recarga
    Criação da Classe recarga com seus atributos, método construtor e o método GetData.

---
+++ EmpresaDeTelefoniaCelular - TP1/src/Recarga.java
@@ -0,0 +1,14 @@
+public class Recarga {
+	
+	private GregorianCalendar  data;
+	private float valor;
+	
+	public Recarga(GregorianCalendar data, float valor) {
+		this.data = data;
+		this.valor = valor;	
+	}
+	
+	public GregorianCalendar getData() {
+		return this.data; 	
+	}
+}
